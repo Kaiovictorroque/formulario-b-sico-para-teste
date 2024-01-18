d54b2156729b0260574660f78182d32a8bd878cb
Kaio Victor Ferreira Roque <kaiovictor28@outlook.com>
2024-01-18 17:59:34 -0300

    atualização

---
+++ Banco de dados/banco de dados - teste2..sql
@@ -0,0 +1,8 @@
+CREATE TABLE `dados` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `idade` int NOT NULL,
+  `cpf` varchar(14) NOT NULL, PRIMARY KEY (`id`)
+) 
+
+ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
